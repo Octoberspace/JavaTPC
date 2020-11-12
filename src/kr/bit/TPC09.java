a646a476f4df588b31697d9634ac8d7442f7f079
Siyeon of the Ham <gkatldus1@gmail.com>
2020-11-12 15:21:28 +0900

    not static method's use

---
+++ src/kr/bit/TPC09.java
@@ -0,0 +1,16 @@
+package kr.bit;
+
+public class TPC09 {
+	public static void main(String[] args) {
+		int a=56;
+		int b=40;
+		
+		TPC09 tpc=new TPC09();
+		int v=tpc.sum(a,b);
+		System.out.println(v);
+	}
+	public int sum(int a, int b) {
+		int v=a+b;
+		return v;
+	}
+}
