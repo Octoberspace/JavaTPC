bf6befa940a6b0bb9da88336930b1b28e944b002
Siyeon of the Ham <gkatldus1@gmail.com>
2020-11-13 14:24:32 +0900

    The meaning of private constructor

---
+++ src/TPC31.java
@@ -0,0 +1,11 @@
+import kr.tpc.Inflearn;
+
+public class TPC31 {
+	public static void main(String[] args) {
+		
+		//Inflearn inf=new Inflearn();
+		Inflearn.tpc();
+		//inf.java();
+		Inflearn.java();
+	}
+}
---
+++ src/kr/tpc/Inflearn.java
@@ -0,0 +1,16 @@
+package kr.tpc;
+
+public class Inflearn {
+	
+	private Inflearn() {
+		
+	}
+	
+	public static void tpc() {
+		System.out.println("TPc강의 너무 재미있다.");		
+	}
+	public static void java() {
+		System.out.println("Java강의 너무 재미있다");
+		
+	}
+}
